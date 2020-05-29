d959c7eef9be66e70c87d66d2361a8f4b4b8f6b3
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-05-29 14:57:35 +0300

    Make sure user is always signed in

--- app/src/main/java/com/example/android/tasks/ui/BaseActivity.java
+++ app/src/main/java/com/example/android/tasks/ui/BaseActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.android.tasks.login.LoginActivity;
+import com.example.android.tasks.utils.FirebaseUserLiveData;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -27,16 +28,22 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        checkUserIsSignedIn();
-    }
-
-    protected void checkUserIsSignedIn() {
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-
-        if (currentUser == null) {
+        if (!isUserSignedIn()) {
             navigateToLoginScreen();
         }
+
+        FirebaseUserLiveData userLiveData = new FirebaseUserLiveData();
+        userLiveData.observe(this, firebaseUser -> {
+            if (!isUserSignedIn()) {
+                navigateToLoginScreen();
+            }
+        });
+    }
+
+    protected boolean isUserSignedIn() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        return currentUser != null;
     }
 
     protected void navigateToLoginScreen() {
